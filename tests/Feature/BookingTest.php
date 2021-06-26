78065f8d544a4d442316708b7ebf6b95f411a226
Maged Raslan <maged.ahmedr@gmail.com>
2021-06-26 18:25:57 +0200

    Booking test draft

---
+++ tests/Feature/BookingTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Dashboard\Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Booking;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class BookingTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_admin_can_view_bookings()
+    {
+        // arrange
+
+        // act
+
+        // view
+    }
+}
